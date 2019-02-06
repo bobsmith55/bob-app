b5731490dcaeee8f408806a11b696f2030eb8f1c
Bob Smith <bob55.smith55@outlook.com>
2019-02-06 09:11:56 +0100

    Updates

---
+++ precache-manifest.2b71f8eb0d339c45c197fcc0b1244c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bob-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b532288a774719addb4",
+    "url": "/bob-app/static/js/runtime~main.2b532288.js"
+  },
+  {
+    "revision": "15f0c314891f0672873d",
+    "url": "/bob-app/static/js/main.15f0c314.chunk.js"
+  },
+  {
+    "revision": "d22e95278b186df6c2a5",
+    "url": "/bob-app/static/js/1.d22e9527.chunk.js"
+  },
+  {
+    "revision": "15f0c314891f0672873d",
+    "url": "/bob-app/static/css/main.774c3393.chunk.css"
+  },
+  {
+    "revision": "09b8c25f978368dd0fa9de796601a4a8",
+    "url": "/bob-app/index.html"
+  }
+];
